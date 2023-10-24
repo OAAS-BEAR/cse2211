ea4e94f8c7b659e0ebb022c8a33f55007d0f9a09
OAAS-BEAR <1147182925@qq.com>
2023-10-23 23:10:56 -0700

    update

--- src/utils/timer_util.h
+++ src/utils/timer_util.h
@@ -7,52 +7,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/resource.h>
+#include <pthread.h>
 #include <unistd.h>
-#include<math.h>
-#include<vector>
-#include<numeric>
-#include <x86intrin.h> 
-
-/*
-unsigned long cycles_high0, cycles_low0, cycles_high1, cycles_low1;
-#define timer_start \
-    asm volatile ("cpuid\n\t" \
-          "rdtsc\n\t" \
-		  "mov %%rdx, %0\n\t" \
-		  "mov %%rax, %1\n\t" \
-		  : "=r" (cycles_high0), "=r" (cycles_low0)\
-          :: "%rax","%rbx","%rcx","%rdx");
-
- 
-#define timer_end \
-    asm volatile ("rdtscp\n\t" \
-		  "mov %%rdx, %0\n\t" \
-		  "mov %%rax, %1\n\t" \
-          "cpuid\n\t" \
-		  : "=r" (cycles_high1), "=r" (cycles_low1) \
-          :: "%rax","%rbx","%rcx","%rdx");
-
-
-unsigned long getTimeDiff(unsigned long cycles_high0, unsigned long cycles_low0, unsigned long cycles_high1, unsigned long cycles_low1){
-        unsigned long start = ((unsigned long long)cycles_high0 << 32) | cycles_low0;
-        unsigned long end = ((unsigned long long)cycles_high1 << 32) | cycles_low1;
-        unsigned long duration = end - start;
-        return duration;
-}
-*/
+#include <vector>
+#include <iostream>
+#include <x86intrin.h> // for rdtsc
+#include <math.h>
+#include <numeric>
 
 const int MAXPRIORITY = -20;
+const int ITERATIONS = 1000;
 
+
+
+// set priority for current process
 void setPriority(int priority=MAXPRIORITY){
     pid_t pid = getpid();
     int older_priority = getpriority(PRIO_PROCESS, pid);
 
     int ret = setpriority(PRIO_PROCESS, pid, priority);
-    std::cout << "return value:"<<ret<< std::endl;
+
     int current_priority = getpriority(PRIO_PROCESS, pid);
-     std::cout << "Desired Priority: " << priority << ", "
-           << "Old Priority: " << older_priority << ", "
-          << "Current Priority: " << current_priority << std::endl;
+    // std::cout << "Desired Priority: " << priority << ", "
+    //       << "Old Priority: " << older_priority << ", "
+    //       << "Current Priority: " << current_priority << std::endl;
     assert(priority == current_priority);
 
     
